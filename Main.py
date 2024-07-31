25273f4c9a5ef4f264ed165278306b61423850ae
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2024-07-31 15:29:39 +0530

    ....

--- Main.py
+++ Main.py
@@ -22,3 +22,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Also if I have already 
+# 1.) Created ec2 instance and installed all the dependencies like python 3.11.3 , Docker etc.
+# 2.) Written the docker code , created a docker image and uploaded that docker image in the docker hub
+# 3.) The docker image when I run it locally is working completely fine 
+# What are the thing which I should do next 
+# tell me the steps of how to run the code in productions 
